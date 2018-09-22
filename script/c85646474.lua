c98091655354146f00b2dc4213e4c79ea2cf7f6e
Aliff Yusairy <38120207+aliffyusairy@users.noreply.github.com>
2018-09-22 14:40:53 +0800

    Add function to Gravekeeper Nobleman Effect to Gravekeeper Nobleman effect from removed to deck
    Signed-off-by: Aliff Yusairy <38120207+aliffyusairy@users.noreply.github.com>

--- script/c85646474.lua
+++ script/c85646474.lua
@@ -5,15 +5,31 @@ function c85646474.initial_effect(c)
 	--e1:SetDescription(aux.Stringid(85646474,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	
 	e1:SetCode(EVENT_LEAVE_FIELD)
-	
-	
 	e1:SetCondition(c85646474.condition)
 	e1:SetTarget(c85646474.target)
 	e1:SetOperation(c85646474.operation)
 	c:RegisterEffect(e1)
+	
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(85646474,0))
+	e2:SetCategory(CATEGORY_TODECK)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_REMOVED)
+	e2:SetCost(c85646474.cost)
+	e2:SetTarget(c85646474.target)
+	e2:SetOperation(c85646474.operation)
+	c:RegisterEffect(e2)
 end
+function c85646474.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then 
+		return true --e:GetHandler():IsAbleToRemoveAsCost() 
+	end
+	Duel.SendtoDeck(e:GetHandler(),nil,0,REASON_EFFECT)
+	Duel.ShuffleDeck(tp)
+end
+
 function c85646474.condition(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsPreviousLocation(LOCATION_ONFIELD)
